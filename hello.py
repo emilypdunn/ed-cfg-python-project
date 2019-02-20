f2c1b591bcd4f909229a13c9441b02ab765183ee
Emily <epdunn1@sheffield.ac.uk>
2019-02-20 19:27:23 +0000

    Add hello
    Added my first repository comment!

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello, World!")
+print(2+4)
+print("Let's hope this works")
+age = 20
+hobby = "eating"
+description = "Her age is {} and she likes {}.".format(age,hobby)
+print(description)
+# Emily's description
+
+# counting the "the"s in the sentence
+thecount = "The quick brown fox jumps over the lazy dog.".count("the")
+print(thecount)
+
+# makes the sentence all lower case so "The" and "the" both count
+sentence = "The quick brown fox jumps over the lazy dog."
+print(sentence.lower().count("the"))
