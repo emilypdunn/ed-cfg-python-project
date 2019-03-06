a08a4ff871c557de1b481056b4aefd45ecb093f9
Emily <epdunn1@sheffield.ac.uk>
2019-03-06 19:42:59 +0000

    WORKSHOP 4 TEMPLATES

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+#@app.route("/")
+# decorator - turns from normal
+#python to function responding to
+#local host
+#def say_hello():
+#  return "Hello world!"
+
+@app.route("/qwerty")
+def qwerty():
+  return "qwerty!"
+
+@app.route("/")
+def say_hello():
+  return render_template("index.html")
+
+@app.route("/<name>")
+def say_hello_to(name):
+  return render_template("index.html", user=name)
+
+# automatically refreshs the page
+# saves from killing and restarting
+# app.run(debug=True)
+
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+  # request.values is a dictionary holding any
+  # POST request data that's not already part of the URL
+  data = request.values
+
+  return render_template("feedback.html", form_data=data)
+
+app.run(debug=True)
---
+++ factorial.py
@@ -0,0 +1,7 @@
+import math
+five_factorial = math.factorial(5)
+#print(five_factorial) # 120
+
+from random import randint
+random_number = randint(1, 10)
+print(random_number) 
