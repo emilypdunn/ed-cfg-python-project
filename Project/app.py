119d0e7b4d46a9c2d296a8fffe7eb891e1c6169c
Emily <epdunn1@sheffield.ac.uk>
2019-04-03 11:05:44 +0100

    nav & months sorted

--- Project/app.py
+++ Project/app.py
@@ -10,30 +10,24 @@ app = Flask(__name__)
 #  return "Hello world!"
 
 @app.route("/")
-def say_hello():
+def iniial_page():
   return render_template("index.html")
 
 @app.route("/<name>")
 def say_hello_to(name):
   return render_template("index.html", user=name)
-# automatically refreshs the page
-# saves from killing and restarting
-# app.run(debug=True)
 
-@app.route("/feedback", methods=["POST"])
-def get_feedback():
-  # request.values is a dictionary holding any
-  # POST request data that's not already part of the URL
-  data = request.values
+@app.route("/female.html")
+def female():
+  return render_template("female.html")
 
-  return render_template("feedback.html", form_data=data)
+@app.route("/aboutUs.html")
+def about_us():
+  return render_template("aboutUs.html")
 
 @app.route("/newpage", methods=["POST"])
 def new_page():
-  # request.values is a dictionary holding any
-  # POST request data that's not already part of the URL
   data = request.values
-
   return render_template("newpage.html", form_data=data)
 
 
